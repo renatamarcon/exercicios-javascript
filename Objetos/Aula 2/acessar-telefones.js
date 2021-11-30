86d70a1b5e8d339386ed18c48dc76c078584a649
renata.marcon <renata.marcon@ntendencia.com.br>
2021-11-30 15:23:06 -0300

    feat: exercicios de objetos

---
+++ Objetos/Aula 2/acessar-telefones.js
@@ -0,0 +1,9 @@
+const cliente = {
+  nome:"Andre",
+  idade:36,
+  cpf:"12543652266",
+  email:"andre@email.com",
+  fones:["55912354978","55219887432"]
+}
+
+cliente.fones.forEach(fone => console.log(fone))
